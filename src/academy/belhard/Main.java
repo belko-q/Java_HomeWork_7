5bbc69acd604c50f0759a4a34e722b5f6ce5f10f
belko-q <>
2020-12-08 11:12:16 +0300

    Update Main.java

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -16,11 +16,11 @@ public class Main {
 
 
         Set<Person> personSet = new HashSet<>();
-        personSet.add((new Person("FirstNameSet1", "LastNameSet1", Gender.MALE)));
-        personSet.add((new Person("FirstNameSet1", "LastNameSet1", Gender.MALE)));
-        personSet.add((new Person("FirstNameSet3", "LastNameSet3", Gender.FEMALE)));
-        personSet.add((new Person("FirstNameSet3", "LastNameSet3", Gender.FEMALE)));
-        personSet.add((new Person("FirstNameSet5", "LastNameSet5", Gender.FEMALE)));
+        personSet.add((new Person("FirstName1", "LastName1", Gender.MALE)));
+        personSet.add((new Person("FirstName1", "LastName1", Gender.MALE)));
+        personSet.add((new Person("FirstName3", "LastName3", Gender.FEMALE)));
+        personSet.add((new Person("FirstName3", "LastName3", Gender.FEMALE)));
+        personSet.add((new Person("FirstName5", "LastName5", Gender.FEMALE)));
 
         for (Person person : personSet)
             System.out.println(person);
@@ -31,21 +31,24 @@ public class Main {
         HashMap<Integer, Person> personMap = new HashMap<>();
 
 
-        personMap.put(1, (new Person("FirstNameSet6", "LastNameSet6", Gender.FEMALE)));
-        personMap.put(2, (new Person("FirstNameSet7", "LastNameSet7", Gender.FEMALE)));
-        personMap.put(3, (new Person("FirstNameSet8", "LastNameSet8", Gender.MALE)));
-        personMap.put(4, (new Person("FirstNameSet9", "LastNameSet9", Gender.FEMALE)));
-        personMap.put(5, (new Person("FirstNameSet10", "LastNameSet10", Gender.MALE)));
+        personMap.put(1, (new Person("FirstName6", "LastName6", Gender.FEMALE)));
+        personMap.put(2, (new Person("FirstName7", "LastName7", Gender.FEMALE)));
+        personMap.put(3, (new Person("FirstName8", "LastName8", Gender.MALE)));
+        personMap.put(4, (new Person("FirstName9", "LastName9", Gender.FEMALE)));
+        personMap.put(5, (new Person("FirstName10", "LastName10", Gender.MALE)));
         System.out.println(personMap);
 
         System.out.println(personMap.size());
 
 
+        Set<Integer> keys = personMap.keySet();
+        System.out.println("Ключи: " + keys);
+
+        ArrayList<Person> values = new ArrayList<>(personMap.values());
+        System.out.println("Значения: " + values);
 
 
     }
 
-    private static HashMap<Integer, Person> getPersonMap(HashMap<Integer, Person> personMap) {
-        return personMap;
-    }
+
 }
